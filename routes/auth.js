bcd9229aa4086ab7ce0549272bf2a65a4d3b101f
DraytonFHSU <ddpletcher@mail.fhsu.edu>
2024-05-05 20:50:58 -0500

    Deleted Login things

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express();
-const authController = require("../controller/authController");
-
-router.post("/", authController.handelLogin);
-
-module.exports = router;
--- routes/register.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express();
-const registerController = require("../controller/registerController");
-
-router.post("/", registerController.handelNewUser);
-
-module.exports = router;
