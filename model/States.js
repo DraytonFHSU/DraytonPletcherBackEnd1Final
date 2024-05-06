15046fbd0672802d7fe8c39a8fc9c26754ea6fbf
DraytonFHSU <ddpletcher@mail.fhsu.edu>
2024-05-06 18:25:32 -0500

    Added Export module

--- model/States.js
+++ model/States.js
@@ -13,37 +13,4 @@ const StateSchema = new Schema({
 }, 
 );
 
-// In the collection, the stateCode property will contain state abbreviation values.
-// In the collection, the funfacts array will contain “fun facts” about the state
-
-
-//   email: {
-//     type: String,
-//   },
-//   roles: {
-//     User: {
-//       type: Number,
-//       default: 2001,
-//     },
-//     Editor: Number,
-//     Admin: Number,
-//   },
-
-// module.exports = mongoose.model("User", UserSchema);
-
-
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// const employeeSchema = new Schema({
-//   firstName: {
-//     type: String,
-//     required: true,
-//   },
-//   lastName: {
-//     type: String,
-//     required: true,
-//   },
-// });
-
-// module.exports = mongoose.model("Employee", employeeSchema);
+module.exports = mongoose.model("State", stateSchema);
